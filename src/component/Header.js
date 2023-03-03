9ef58fe5e886a60f94934679931010520eab1441
Faizan <faizansayyed2911@gmail.com>
2023-03-03 19:11:19 +0530

    initial commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header_icon' src="https://i.pinimg.com/originals/1c/5e/94/1c5e94cfe604f37222bdbe24d6aa6b00.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Header
