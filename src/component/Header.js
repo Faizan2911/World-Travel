460fadb300f1abda4696dbdeb24d160a92958da4
Faizan <faizansayyed2911@gmail.com>
2023-03-05 17:01:36 +0530

    added the banner

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,7 +12,7 @@ function Header() {
             <img className='header_icon' src="https://i.pinimg.com/originals/1c/5e/94/1c5e94cfe604f37222bdbe24d6aa6b00.jpg" alt="" />
 
             <div className='header_center'>
-                <input type="text" />
+                <input type="text" placeholder='Search by city or region, hotel, or place' />
                 <SearchIcon />
             </div>
 
